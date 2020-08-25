32ad80d409c052ce1f68944b24d62a449463f875
Roman Zhigaikov <zhigaikov.roman@yandex.ru>
2020-08-25 18:30:12 +0300

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,9 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h2>Home page</h2>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dignissimos eveniet excepturi expedita impedit numquam quae quas tenetur velit voluptas!</p>
+
+    <router-link to="/todos">Todos</router-link>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
---
+++ src/views/Todos.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h2>Test</h2>
+    <router-link to="/">Home</router-link>
+    <hr />
+    <AddTodo @add-todo="addTodo" />
+    <hr />
+    <TodoList v-bind:todos="todos" @remove-todo="removeTodo" />
+  </div>
+</template>
+
+<script>
+import TodoList from "@/components/TodoList";
+import AddTodo from "@/components/AddTodo";
+export default {
+  name: "App",
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  mounted() {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=7")
+      .then((response) => response.json())
+      .then((json) => {
+        this.todos = json;
+      });
+  },
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter((x) => x.id !== id);
+    },
+    addTodo(todo) {
+      this.todos.push(todo);
+    },
+  },
+  components: {
+    TodoList,
+    AddTodo,
+  },
+};
+</script>
